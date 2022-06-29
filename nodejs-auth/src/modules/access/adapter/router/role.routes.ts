5222ceb7f4dd985cd52a8fe4e2f5382ca2b03e10
Ivano Carvalho <ivano.carvalho@blueshift.com.br>
2022-06-29 15:43:29 -0300

    others

--- nodejs-auth/src/modules/access/adapter/router/role.routes.ts
+++ nodejs-auth/src/modules/access/adapter/router/role.routes.ts
@@ -1,4 +1,5 @@
 import ensureAthenticated from '@common/infra/http/middlewares/ensure-authenticated';
+import ensurePermission from '@common/infra/http/middlewares/ensure-permission';
 import { Router } from 'express';
 import RoleController from '../controller/RoleController';
 
@@ -7,12 +8,12 @@ const roleController = new RoleController();
 
 roleRouter.use(ensureAthenticated);
 
-roleRouter.post( '/', roleController.create );
-roleRouter.put( '/', roleController.update );
-roleRouter.get( '/', roleController.find );
-roleRouter.get( '/filter', roleController.filter );
-roleRouter.get( '/:roleId', roleController.findById );
-roleRouter.delete( '/:roleId', roleController.delete );
+roleRouter.post( '/', ensurePermission(['CREATE_ROLE']), roleController.create );
+roleRouter.put( '/', ensurePermission(['EDIT_ROLE']), roleController.update );
+roleRouter.get( '/', ensurePermission(['FIND_ROLE']), roleController.find );
+roleRouter.get( '/filter', ensurePermission(['FIND_ROLE']), roleController.filter );
+roleRouter.get( '/:roleId', ensurePermission(['FIND_ROLE']), roleController.findById );
+roleRouter.delete( '/:roleId', ensurePermission(['CREATE_DELETE']), roleController.delete );
 
 export default roleRouter;
 
--- nodejs-auth/src/modules/actor/adapter/router/actor.routes.ts
+++ nodejs-auth/src/modules/actor/adapter/router/actor.routes.ts
@@ -1,4 +1,5 @@
 import ensureAthenticated from '@common/infra/http/middlewares/ensure-authenticated';
+import ensurePermission from '@common/infra/http/middlewares/ensure-permission';
 import { Router } from 'express';
 import ActorController from '../controller/ActorController';
 
@@ -6,11 +7,11 @@ const actorRouter = Router();
 const actorController = new ActorController();
 
 actorRouter.use(ensureAthenticated);
-actorRouter.post('/', actorController.save);
-actorRouter.put('/', actorController.save);
-actorRouter.get('/', actorController.find);
-actorRouter.get('/emails/:email', actorController.findByEmail);
-actorRouter.get('/:actorId', actorController.findById);
+actorRouter.post('/', ensurePermission(['CREATE_ACTOR']), actorController.save);
+actorRouter.put('/', ensurePermission(['EDIT_ACTOR']), actorController.save);
+actorRouter.get('/', ensurePermission(['FIND_ACTOR']), actorController.find);
+actorRouter.get('/emails/:email', ensurePermission(['FIND_ACTOR']), actorController.findByEmail);
+actorRouter.get('/:actorId', ensurePermission(['FIND_ACTOR']), actorController.findById);
 
 
 export default actorRouter;
